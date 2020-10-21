4ed2bafddfbd6819a11d8e5742124036db547553
KESHAV GUPTA <59222869+keshavgupta848101@users.noreply.github.com>
2020-10-21 21:16:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function change()
+{
+
+    var input = document.querySelector("input").value;
+    console.log(input);
+    var reflect = document.querySelector("#reflect");
+    reflect.innerHTML = input;
+
+}
+
